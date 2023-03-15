67c8b23b74cf1cd3cb4611db540901b2481ab393
power124674321 <82689656+power124674321@users.noreply.github.com>
2023-03-14 19:02:09 -0500

    Add files via upload

---
+++ spectagram-stage-1-master 2/Config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyBQj4swhRyYLBpySdbtbZ4xjD6e-C9Y1m8",
+    authDomain: "induthebot21422.firebaseapp.com",
+    projectId: "induthebot21422",
+    storageBucket: "induthebot21422.appspot.com",
+    messagingSenderId: "225973338896",
+    appId: "1:225973338896:web:eab32ace093144d6859b95",
+    measurementId: "G-YYQPCNHLTE"
+  };
